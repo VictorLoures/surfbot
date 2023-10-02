a8699202b151a180795ff0e90e1b7cad28569cb8
Victor <victor.loures@projetusti.com.br>
2023-10-02 18:11:46 -0300

    Ajuste para achar rota

--- surfbot-backend/src/main/java/br/com/surfbot/surfbotbackend/controller/EmailController.java
+++ surfbot-backend/src/main/java/br/com/surfbot/surfbotbackend/controller/EmailController.java
@@ -1,4 +1,4 @@
-package controllers;
+package br.com.surfbot.surfbotbackend.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ surfbot-backend/src/main/java/br/com/surfbot/surfbotbackend/controller/package-info.java
@@ -0,0 +1,1 @@
+package br.com.surfbot.surfbotbackend.controller;
